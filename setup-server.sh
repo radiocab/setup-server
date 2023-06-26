bf518217e9042535b448713e15548d59b204f532
radiocab <136172056+radiocab@users.noreply.github.com>
2023-06-26 10:20:35 +0300

    Update setup-server.sh

--- setup-server.sh
+++ setup-server.sh
@@ -5,12 +5,15 @@ apt -qq update
 apt-get -qq install sudo -y
 export zipname="common-linux-scripts"
 
+#--no-verbose
 cd $HOME
-wget --no-verbose https://github.com/radiocab/setup-server/raw/main/$zipname.zip  -O $HOME/
- 
+echo downloading main scripts
+wget --no-verbose https://github.com/radiocab/setup-server/raw/main/$zipname.zip  -O $HOME/$zipname.zip
+echo downloaded
+
 sudo apt-get -qq install zip -y
 
 echo $0
-unzip  -P $1 -d $HOME/test/ -x "setup-server.sh" $zipname.zip; cp -r --update $HOME/test/$zipname/* $HOME/test/ ; rm -rf $HOME/test/zipname/
+unzip  -P $1 -d $HOME/test/  $zipname.zip -x $zipname"/setup-server.sh"; cp -r --update $HOME/test/$zipname/* $HOME/test/ ; rm -rf $HOME/test/$zipname/
 
 exit 0
