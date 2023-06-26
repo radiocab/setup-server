58109267def14d3451ddc69b624c75268245cb0f
radiocab <136172056+radiocab@users.noreply.github.com>
2023-06-26 09:48:27 +0300

    Update setup-server.sh

--- setup-server.sh
+++ setup-server.sh
@@ -3,13 +3,14 @@
 # install sudo : 
 apt -qq update
 apt-get -qq install sudo -y
+export zipname="common-linux-scripts"
 
-
-#wget --no-verbose https://raw.githubusercontent.com/simotek/scripts-config/master/.colors -O $HOME/.colors 
-
+cd $HOME
+wget --no-verbose https://github.com/radiocab/setup-server/raw/main/$zipname.zip  -O $HOME/
+ 
 sudo apt-get -qq install zip -y
 
 #echo 'test1test'> pass
-unzip  -P test1test -d $HOME/test/ -x "setup-server.sh" common-linux-scripts.zip; cp -r --update $HOME/test/common-linux-scripts/* $HOME/test/ ; rm -rf $HOME/test/common-linux-scripts/
+unzip  -P test1test -d $HOME/test/ -x "setup-server.sh" $zipname.zip; cp -r --update $HOME/test/$zipname/* $HOME/test/ ; rm -rf $HOME/test/zipname/
 
 exit 0
